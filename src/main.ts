d9250ae95521df6547fe768beb5f2d58b3076187
aom.chatchalerm <aom.chatchalerm@gmail.com>
2025-10-07 17:25:08 +0700

    s

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
-    .setTitle('TESA GPS Tracking Services')
+    .setTitle('TESA GPS Tracking Service')
     .setDescription(
       'API for GPS tracking and object detection with real-time communication',
     )
