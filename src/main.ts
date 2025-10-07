ddadbadf3178e6fd51c0a22cb7af883cddc5ca4d
aom.chatchalerm <aom.chatchalerm@gmail.com>
2025-10-07 20:36:12 +0700

    s

--- src/main.ts
+++ src/main.ts
@@ -19,17 +19,17 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  const config = new DocumentBuilder()
-    .setTitle('TESA GPS Tracking Services DPxxx')
-    .setDescription(
-      'API for GPS tracking and object detection with real-time communication',
-    )
-    .setVersion('1.0')
-    .addTag('object-detection', 'Object detection endpoints')
-    .build();
+  // const config = new DocumentBuilder()
+  //   .setTitle('TESA GPS Tracking Services DPxxx')
+  //   .setDescription(
+  //     'API for GPS tracking and object detection with real-time communication',
+  //   )
+  //   .setVersion('1.0')
+  //   .addTag('object-detection', 'Object detection endpoints')
+  //   .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
